9a361a4aa0faddee0eef320dcdd531d5e789c8a3
Daniel Halachev <dani22h09_pl@abv.bg>
2024-06-05 20:01:54 +0300

    Added source code and screenshots for HW03

--- SourceCode/Ray.cpp
+++ SourceCode/Ray.cpp
@@ -30,7 +30,10 @@ Ray::Ray() = default;
 Ray::Ray(const Vector& origin, const Vector& direction) : origin(origin), direction(direction){};
 
 Color Ray::generateColor() const {
-  return Color(static_cast<int>(std::abs(this->direction.x * 2)) % 255,
-               static_cast<int>(std::abs(this->direction.y * 2)) % 255,
-               static_cast<int>(std::abs(this->direction.z * 2)) % 255);
+  // return Color(static_cast<int>(std::abs(this->direction.x * 2)) % 255,
+  //              static_cast<int>(std::abs(this->direction.y * 2)) % 255,
+  //              static_cast<int>(std::abs(this->direction.z * 2)) % 255);
+  return Color(static_cast<int>(std::abs(this->direction.x) * 255) % 255,
+               static_cast<int>(std::abs(this->direction.y) * 255) % 255,
+               static_cast<int>(std::abs(this->direction.z) * 255) % 255);
 }
--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -2,7 +2,7 @@
 
 #include "RayTracer.h"
 int main() {
-  std::vector<Image> images = {{1600, 900}, {1920, 1080}, {400, 300}, {1920, 1920}};
+  std::vector<Image> images = {{3840, 2160}, {1600, 900}, {1920, 1080}, {400, 300}, {1920, 1920}};
   for (auto &image : images) {
     RayTracer rayTracer(image);
     rayTracer.generatePPM("/home/daniel/tracer" + std::to_string(image.width) + "x" + std::to_string(image.height) +
