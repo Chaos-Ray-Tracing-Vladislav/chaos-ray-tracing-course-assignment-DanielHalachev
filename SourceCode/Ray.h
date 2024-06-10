3e276c629a7af4bc150fa0992ae57e8e872e8ad3
Daniel Halachev <dani22h09_pl@abv.bg>
2024-06-10 13:44:40 +0300

    Added source code and screenshots for HW04

--- SourceCode/Ray.h
+++ SourceCode/Ray.h
@@ -9,6 +9,10 @@ struct Vector {
   Vector(float x, float y, float z);
   Vector operator-(const Vector &other) const;
   Vector operator+(const Vector &other) const;
+  Vector operator*(const Vector &other) const;
+  Vector operator*(float scalar) const;
+  friend Vector operator*(float lhs, const Vector &rhs);
+  friend std::ostream &operator<<(std::ostream &os, const Vector &vector);
   void normalize();
   float length() const;
 };
